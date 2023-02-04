836a163d73cb17ad0f11b9c4765befbda2a2c176
Khuat Viet Hoa <kv@rida.ai>
2023-02-04 10:25:06 +0800

    updated

--- app/src/main/java/com/example/algorithm/MainActivity.kt
+++ app/src/main/java/com/example/algorithm/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.algorithm.array.CountSubArray
 import com.example.algorithm.array.MinAbsoluteSumPair
+import com.example.algorithm.array.MinimumAbsoluteDifference
 import com.example.algorithm.hastable.UniqueCharacterInString
 import kotlin.math.abs
 
@@ -15,14 +16,22 @@ class MainActivity : AppCompatActivity() {
     private val minAbsoluteSumPair by lazy { MinAbsoluteSumPair() }
     private val uniqueCharacterInString by lazy { UniqueCharacterInString() }
     private val countSubArray by lazy { CountSubArray() }
+    private val minimumAbsoluteDifference by lazy { MinimumAbsoluteDifference() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        testCountSubArray()
+        testMinimumAbsoluteDifference()
+        //testCountSubArray()
         //testUniqueCharacterInString()
         //testMinAbsSumPair()
+    }
+
+    private fun testMinimumAbsoluteDifference() {
+        println(minimumAbsoluteDifference.execute(listOf(5, 3, 2, 4)))
+        println(minimumAbsoluteDifference.execute(listOf(2, 4, 7, 13, 17)))
+        println(minimumAbsoluteDifference.execute(listOf(1, 7, -12, 22, -8, -16, 18)))
     }
 
     private fun testCountSubArray() {
--- app/src/main/java/com/example/algorithm/array/MinimumAbsoluteDifference.kt
+++ app/src/main/java/com/example/algorithm/array/MinimumAbsoluteDifference.kt
@@ -1,7 +1,10 @@
 package com.example.algorithm.array
 
+import kotlin.math.abs
+
 /**
- * Medium
+ * Medium - Minimum Absolute Difference
+ *
  * You are given an array of distinct integers arr[],
  * write a program to find all pairs of elements such that their absolute difference is minimum.
  * Return a list of pairs in ascending order (with respect to pairs), each pair [i, j] follows:
@@ -22,12 +25,73 @@ package com.example.algorithm.array
  * Example 3
  * Input: arr[] = [1, 7, -12, 22, -8, -16, 18]
  * Output: [[-12, -8],[-12, -16],[22, 18]]
- * Explanation: All these pairs have the same minimum absolute difference.Ï
+ * Explanation: All these pairs have the same minimum absolute difference.
  */
 class MinimumAbsoluteDifference {
 
+    /**
+     * Solution
+     * 1. Sort array
+     * 2. Find minimum absolute diff number
+     * 2. Find the pair which right minimum
+     *
+     * Time Complexity: O(nlogn) (Quick Sort) (the complexity depend on Sort method)
+     * Auxiliary Space : O(n/2)
+     */
+    fun execute(input: List<Int>): List<Pair<Int, Int>> {
+        if (input.isEmpty()) {
+            return emptyList()
+        }
+        if (input.size == 1) {
+            return emptyList()
+        }
 
-    fun minimumAbsoluteDifference() {
+        //val sortedArray = input.sorted()
+        val sortedArray = selectionSort(input.toTypedArray())
+        var minAbsNumber = Int.MAX_VALUE
+        for (i in 1 until sortedArray.size) {
+            val min = abs(sortedArray[i] - sortedArray[i - 1])
+            if (min < minAbsNumber) {
+                minAbsNumber = min
+            }
+        }
 
+        val result = arrayListOf<Pair<Int, Int>>()
+        for (i in 1 until sortedArray.size) {
+            val min = abs(sortedArray[i] - sortedArray[i - 1])
+            if (min == minAbsNumber) {
+                result.add(Pair(sortedArray[i - 1], sortedArray[i]))
+            }
+        }
+
+        return result
+    }
+
+    private fun selectionSort(input: Array<Int>): Array<Int> {
+        if (input.isEmpty() || input.size == 1) {
+            return input
+        }
+
+        for (i in 0 until input.size - 1) {
+            for (j in i until input.size) {
+                if (input[i] > input[j]) {
+                    permutation(input, i, j)
+                }
+            }
+        }
+
+        return input
+    }
+
+    private fun permutation(input: Array<Int>, i: Int, j: Int) {
+        if (i < 0 || j < 0) {
+            return
+        }
+        if (i >= input.size || j >= input.size) {
+            return
+        }
+        val temp = input[i]
+        input[i] = input[j]
+        input[j] = temp
     }
 }
